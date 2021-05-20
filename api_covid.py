88e4990603a694b4608501da62ac404ca279abdc
solanods <73252886+solanods@users.noreply.github.com>
2021-05-20 12:18:11 -0300

    Add files via upload

--- api_covid.py
+++ api_covid.py
@@ -2,9 +2,15 @@
 import pandas as pd
 from pandas.io.sql import to_sql
 from sqlalchemy import create_engine, types
+import sqlalchemy
 import json
 import requests
 from itertools import chain
+import plotly.express as px
+import mysql.connector
+
+
+
 
 # URL para fazer a requisição da API.
 url = "https://covid-api.mmediagroup.fr/v1/cases"
@@ -15,16 +21,31 @@ retorno = requests.request("GET", url)
 dados = retorno.text
 # Carregamos e transformamos o texto para o formato Json com a função loads() da biblioteca json.
 dados = json.loads(dados)
-
-# descompactando os valores do dict 'dados' com a função chain do módulo itertools
+# descompactando os valores do dict 'dados.values()' com a função chain do módulo itertools
 df = pd.DataFrame(chain.from_iterable(map(lambda sec: sec.values(), dados.values())))
-
 # remover as colunas que não usaremos
 df = df.drop(columns=['sq_km_area', 'life_expectancy', 'elevation_in_meters','continent', 'abbreviation', 'iso', 'capital_city'])
+#remover as linhas Nan do campo country
+df = df.dropna(subset=['country'])
+# formatar o campos updated para o YYYY-MM-DD HH:MM:SS
+df['updated'] = pd.to_datetime(df['updated'])
+# converter coluna "population" to int64 dtype
+df = df.astype({"population": int})
+
+# iniciando a conexão com o banco
+# a strig passada na função abaixo           user  pass        schema
+conn = create_engine('mysql+mysqlconnector://root:''@localhost/projeto_api', connect_args={'auth_plugin': 'mysql_native_password'})
+# Método to_sql transforma o DataFrame em um insert automaticamente, passando a engine criada acima para conectar ao banco
+to_sql(df, 'api_covid', conn, schema='projeto_api', if_exists='append')
+
+
+
+
+
+
 
 # falta fazer!!!
-
-# formatar os campos updated e population
+# tratar valores ausentes
 # fazer as métricas
 # talvez agrupar por location
 # após limpos e tratados mandar para o banco
